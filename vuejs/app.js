7c8c8860333d9cdfa872ea9b98e781f809f0ec93
nidhibavadiya <nidhib@zignuts.com>
2022-11-09 10:33:03 +0530

    images

---
+++ vuejs/app.js
@@ -0,0 +1,27 @@
+new Vue({                       //first creat vue instance 
+    el:'#vue_ex',                        // el property connect with div using id control div 
+    data:{ 
+        name:'nidhi',
+        l_name:'bavadiya',
+        website:'https://www.w3school.com' ,              // data binding v-bind  data of different attribute
+        age:25,
+        link:'<a href="https://www.w3school.com"> w3school website</a>',
+        x:0,
+        y:0,
+    },                              // data property store data of instance 
+    methods:{                               //create method 
+        demo:function(time){                    //create function in method return function value 
+            return 'good' +' '+ time +' '+ this.name;                 // it concat with function perameter / ' ' define space / this keyword define the instance
+        },
+        add:function(inc){
+            this.age+=inc;
+        },
+        sub:function(){
+            this.age--;
+        },
+        mouse_function:function(event){
+            this.x=event.offsetx
+            this.y=event.offsety
+        }
+    }
+});
